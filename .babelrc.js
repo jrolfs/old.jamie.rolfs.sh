a9dc3dd3e048a23385da94d739360bd82423c1ca
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-10-13 01:01:21 -0700

    Format Babel configuration properly

--- .babelrc.js
+++ .babelrc.js
@@ -2,28 +2,30 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  "presets": [
-    "react",
-    ["env", {
-      "modules": false,
-      "debug": true,
-      "useBuiltIns": true,
-      "targets": {
-        "browsers": fs.readFileSync(path.join(__dirname, '.browserslistrc'), 'utf8').split('\n')
+  presets: [
+    'react',
+    [
+      'env',
+      {
+        modules: false,
+        debug: true,
+        useBuiltIns: true,
+        targets: {
+          browsers: fs.readFileSync(path.join(__dirname, '.browserslistrc'), 'utf8').split('\n')
+        }
       }
-    }]
+    ],
+    'next/babel'
   ],
-  "plugins": [
-    "lodash",
-    "transform-flow-strip-types",
-    "transform-class-properties",
-    ["styled-components", { "ssr": true }]
+  plugins: [
+    'lodash',
+    'transform-flow-strip-types',
+    'transform-class-properties',
+    ['styled-components', { ssr: true }]
   ],
-  "env": {
-    "production": {
-      "plugins": [
-        ["styled-components", { "ssr": true, "displayName": false }]
-      ]
+  env: {
+    production: {
+      plugins: [['styled-components', { ssr: true, displayName: false }]]
     }
   }
-}
+};
