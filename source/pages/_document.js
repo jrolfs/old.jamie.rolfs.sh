19d8ce783b05ab97d5749e0591b0ef212c19de86
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-10-13 01:05:34 -0700

    Move analytics to component, only load in production

--- source/pages/_document.js
+++ source/pages/_document.js
@@ -6,6 +6,8 @@ import * as React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+import Analytics from '../components/Analytics';
+
 import { injectGlobalStyles } from '../styles/';
 
 export default class MyDocument extends Document {
@@ -65,14 +67,7 @@ export default class MyDocument extends Document {
             content="https://f001.backblazeb2.com/file/favicon/browserconfig.xml?v=2bQWoe5z5r"
           />
 
-          { /* eslint-disable */ }
-          <script dangerouslySetInnerHTML={{ __html: `
-            !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0";
-              analytics.load("MIG6odzGDzdpFg6QdK6yPIJ1pjqjLy9x");
-              analytics.page();
-            }}();
-          ` }} />
-          { /* eslint-disable */ }
+          {process.env.NODE_ENV === 'production' && <Analytics />}
 
           <meta name="theme-color" content="#ffffff" />
         </Head>
